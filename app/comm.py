a984676d7c3fb2a3b71c131af6e3241b73071eca
ttanh00123 <ttanh0006@gmail.com>
2026-03-13 14:20:23 +0800

    changed model

--- app/comm.py
+++ app/comm.py
@@ -195,7 +195,7 @@ async def generate(request: Request):
         response = await loop.run_in_executor(
             executor,
             lambda: client.chat.completions.create(
-                model="meta-llama/Llama-3.2-3B-Instruct",
+                model="meta-llama/Llama-3.1-8B-Instruct:novita",
                 messages=[
                     {
                         "role": "system", 
